9b94193891cf4b2094c3780aaa6760420e54cd72
David Allen <spondicious@me.com>
2023-12-29 14:30:25 +0100

    Initial Commit

---
+++ swifty1/swifty1App.swift
@@ -0,0 +1,17 @@
+//
+//  swifty1App.swift
+//  swifty1
+//
+//  Created by David Allen on 29/12/23.
+//
+
+import SwiftUI
+
+@main
+struct swifty1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
